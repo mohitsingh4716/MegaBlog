0c396ee1d1647b698cecf795a7271f197e6e31bb
mohitsingh4716 <mohitsingh4716@gmail.com>
2024-06-26 19:03:05 +0530

    Build Authentication Services

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import conf from "./conf/conf.js"
+import './App.css'
+
+function App() {
+  // console.log(conf);
+  
+
+  return (
+    <>
+      <h1>A blog app with appwrite </h1>
+    </>
+  )
+}
+
+export default App
